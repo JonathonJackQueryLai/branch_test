aa0b4312b79c66037b78094479edac762a25260c
sunset <34015040+JonathonJackQueryLai@users.noreply.github.com>
2023-06-19 10:25:56 +0800

    dev： modify a  file

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     print("test")
     print("MAIN")
-    print("test")
+    print("dev")
 
 
 # Press the green button in the gutter to run the script.
